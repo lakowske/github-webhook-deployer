d030a512a93e89dc1012dba0dd42ff1c73116e6c
Seth Lakowske <lakowske@gmail.com>
2015-01-07 08:23:19 -0600

    added version test

--- test.js
+++ test.js
@@ -8,6 +8,8 @@ var Push     = require('github-push-event');
 var fs       = require('fs');
 var crypto   = require('crypto');
 var bl       = require('bl');
+var http     = require('http');
+var git      = require('git-rev');
 
 function signBlob (key, blob) {
   return 'sha1=' +
@@ -56,3 +58,59 @@ test('can receive push events', function(t) {
     });
 })
 
+test('can send current version', function(t) {
+    var dataSign = 'faked';
+    var event    = 'faked';
+
+    //read in event template
+    var event = fs.readFileSync('pushEvent.txt');
+    var b     = bl(function(err, data) {
+        dataSig   = signBlob('testSecret', data);
+        event = data;
+    })
+
+    b.write(event);
+    b.end();
+
+    var port = 3334;
+    var path = '/webhook';
+
+    var deployer = new Deployer({ path : path, secret : 'testSecret' });
+
+    deployer.listen(port);
+
+    var req = http.request({
+        hostname : 'localhost',
+        port     : port,
+        path     : '/version',
+        method   : 'GET',
+    }, function(res) {
+
+        console.log('STATUS: '  + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+        res.setEncoding('utf8');
+        var responseString = '';
+
+        res.on('data', function(chunk) {
+            console.log('BODY: ' + chunk);
+            responseString += chunk.toString();
+        })
+
+        res.on('end', function() {
+            git.long(function(version) {
+                t.strictEqual(responseString, version, 'versions should match');
+                t.end();
+            })
+        })
+
+        res.on('error', function(error) {
+            console.log('got an error');
+            console.log(error);
+        })
+
+    });
+
+    req.end();
+
+})
